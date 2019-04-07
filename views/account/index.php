aca6ab3c31420910637b35292537d5edad4612b8
Aleksandr <ak@symflex.ru>
2019-04-07 15:20:44 +0500

    save

--- views/account/index.php
+++ views/account/index.php
@@ -17,9 +17,32 @@
         text-align: center;
     }
 </style>
+<script>
+    $(function() {
+        $('#create').on('click', function (e) {
+            e.preventDefault();
+
+            let template = '<tr>' +
+                '        <td>' +
+                '        </td>' +
+                '        <td>' +
+                '            <input class="form-control input-sm" name="email" value="">' +
+                '        </td>' +
+                '        <td>' +
+                '            <input class="form-control input-sm" name="apikey" value="">' +
+                '        </td>' +
+                '        <td class="act">' +
+                '            <a class="btn btn-success btn-sm"><i class="glyphicon glyphicon-floppy-saved"></i> </a>' +
+                '        </td>' +
+                '    </tr>';
+            $('table tbody').prepend(template);
+
+        });
+    });
+</script>
 
 <div class="create">
-    <a href="/account/create" class="btn btn-success btn-sm">Добавить</a>
+    <a id="create" class="btn btn-success btn-sm">Добавить</a>
 </div>
 
 <table class="table table-bordered table-hover table-striped">
