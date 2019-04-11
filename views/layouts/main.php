ec1a4636f1c11b07dcc4ba4655ad0017ed308634
Aleksandr <ak@symflex.ru>
2019-04-11 23:44:12 +0500

    save

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -32,7 +32,7 @@ AppAsset::register($this);
     <?php
     NavBar::begin([
         'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
+        'brandUrl' => '/domains',
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
